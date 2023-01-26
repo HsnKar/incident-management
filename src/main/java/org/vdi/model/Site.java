055ff57a817a84d2b9d0794441c09077cafb2a81
HsnKar <robinmacisland@gmail.com>
2023-01-26 16:27:52 +0300

    Added the longitude and the latitude columns

--- src/main/java/org/vdi/model/Site.java
+++ src/main/java/org/vdi/model/Site.java
@@ -1,15 +1,22 @@
 package org.vdi.model;
 
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
-public class Site{
+public class Site extends PanacheEntityBase {
     @Id
     @GeneratedValue
-    private Long id;
+    public Long id;
 
-    private String name;
+    public String name;
+
+    public Long latitude;
+
+    public Long longitude;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "site")
     private List<Incident> incident;
 
@@ -27,6 +34,22 @@ public class Site{
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Long latitude) {
+        this.latitude = latitude;
+    }
+
+    public Long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Long longitude) {
+        this.longitude = longitude;
     }
 
     public List<Incident> getIncident() {
