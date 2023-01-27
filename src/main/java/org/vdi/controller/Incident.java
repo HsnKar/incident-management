1792f379441341c7624274b56298088b9d314f85
HsnKar <robinmacisland@gmail.com>
2023-01-27 10:15:46 +0300

    Handled some exception and added the Map.

--- src/main/java/org/vdi/controller/Incident.java
+++ src/main/java/org/vdi/controller/Incident.java
@@ -1,6 +1,7 @@
 package org.vdi.controller;
 
 import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateException;
 import io.quarkus.qute.TemplateInstance;
 import org.vdi.model.Service;
 import org.vdi.repository.IncidentRepository;
@@ -10,9 +11,12 @@ import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.sql.Date;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Path("/incidents")
 public class Incident {
@@ -41,16 +45,78 @@ public class Incident {
     IncidentRepository incidentRepository;
 
     @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance getAllIncidentView()
+            throws TemplateException {
+        List<org.vdi.model.Incident> incidents = org.vdi.model.Incident.listAll();
+        return listIncident.data(Map.of("incidents", incidents));
+    }
+
+    @GET
+    @Path("/create")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance createUserView()
+            throws TemplateException, IOException {
+        org.vdi.model.Incident incident = new org.vdi.model.Incident();
+        Map<String, Object> obj = new HashMap<>();
+        obj.put("incident", incident);
+        obj.put("isUpdate", false);
+        return addIncident.data(obj);
+    }
+
+
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("/create")
+    public TemplateInstance createIncident
+            (@FormParam("cause") String cause,
+             @FormParam("date_deb") Date date_deb,
+             @FormParam("date_fin") Date date_fin,
+             @FormParam("service") Service service)
+            throws TemplateException {
+        org.vdi.model.Incident incident = new org.vdi.model.Incident();
+        incident.setCause(cause);
+        incident.setDate_deb(date_deb);
+        incident.setDate_fin(date_fin);
+        incident.service = service;
+        org.vdi.model.Incident.persist(incident);
+        return getAllIncidentView();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   /* @GET
     public TemplateInstance get() {
         List<Incident> incidents = incidentRepository.listAll();
         return addIncident.data("incidents", incidents);
     }
-
+*/
+/*
     @GET
     @Path("/list")
     public TemplateInstance geta() {
         return listIncident.data("listIncident", org.vdi.model.Incident.listAll());
     }
+*/
 
 
 /*    @POST
@@ -72,4 +138,7 @@ public class Incident {
     public void createIncident(@FormParam("cause") String cause, @FormParam("resolution") String resolution, @FormParam("date_deb") java.sql.Date date_deb, @FormParam("service") Service service) {
         org.vdi.model.Incident.create(cause, resolution, date_deb, service).persist();
     }*/
+
+
+
 }
