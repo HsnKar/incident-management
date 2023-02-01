79e2539bd7f4146d5c715abeffa1623493f41d28
HsnKar <robinmacisland@gmail.com>
2023-02-01 17:09:32 +0300

    Added the footer Template and the createSite() method to create a Site

--- src/main/java/org/vdi/controller/Site.java
+++ src/main/java/org/vdi/controller/Site.java
@@ -8,9 +8,8 @@ import io.smallrye.common.annotation.Blocking;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import java.time.LocalDate;
 import java.util.*;
 @Path("/sites")
 public class Site {
@@ -21,8 +20,11 @@ public class Site {
     @Inject
     Template listSite;
 
+    @Inject
+    Template footer;
+
     @GET
-    public TemplateInstance get() {
+    public TemplateInstance getSiteForm() {
         org.vdi.model.Site site = new org.vdi.model.Site();
         List<Site> sites = new ArrayList<>();
         return addSite.data("sites", sites);
@@ -30,14 +32,53 @@ public class Site {
 
 
     @POST
-    @Blocking
+    @Path("/postsite")
     @Transactional
-    @Route(methods = Route.HttpMethod.POST, path = "/postsite")
-    public void createSite(RoutingExchange rc) {
+    public TemplateInstance createSite(@FormParam("name") String name,
+                               @FormParam("longitude") long longitude,
+                               @FormParam("latitude") long latitude,
+                               @FormParam("budgetAttachement") long budgetAttachement,
+                               @FormParam("date_MES_2G") String dms2G,
+                               @FormParam("date_MES_3G") String dms3G,
+                               @FormParam("date_MES_4G") String dms4G,
+                               @FormParam("date_MES_4Gplus") String dms4Gplus,
+                               @FormParam("date_MES_4G_TDD") String dms4Gtdd,
+                               @FormParam("typologie") String typologie,
+                               @FormParam("towerType") String towerType,
+                               @FormParam("secteur") String secteur,
+                               @FormParam("commune") String commune,
+                               @FormParam("district") String district,
+                               @FormParam("region") String region,
+                               @FormParam("zoneOrange") String zoneOrange,
+                               @FormParam("mutualite") String mutualite,
+                               @FormParam("proprioTrans") String proprioTrans) {
         org.vdi.model.Site site = new org.vdi.model.Site();
-        site.setName(rc.request().getFormAttribute("name"));
+        site.setName(name);
+        site.setLongitude(longitude);
+        site.setLatitude(latitude);
+        site.setBudgetAttachement(budgetAttachement);
+        if (!dms2G.isEmpty()){
+            site.setDate_MES_2G(LocalDate.parse(dms2G));
+        }
+        if (!dms3G.isEmpty())
+        site.setDate_MES_3G(LocalDate.parse(dms3G));
+        if (!dms4G.isEmpty())
+        site.setDate_MES_4G(LocalDate.parse(dms4G));
+        if (!dms4Gplus.isEmpty())
+        site.setDate_MES_4G_plus(LocalDate.parse(dms4Gplus));
+        if (!dms4Gtdd.isEmpty())
+        site.setDate_MES_4G_TDD(LocalDate.parse(dms4Gtdd));
+        site.setTypologie(typologie);
+        site.setTowerType(towerType);
+        site.setSecteur(secteur);
+        site.setCommune(commune);
+        site.setDistrict(district);
+        site.setRegion(region);
+        site.setZoneOrange(zoneOrange);
+        site.setMutualite(mutualite);
+        site.setProprioTrans(proprioTrans);
         site.persist();
-        rc.response().end("Site inserted");
+        return getSiteForm();
     }
 
 }
