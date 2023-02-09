629008cf713c9b4a20720a5b46e9b2cb942d0c74
HsnKar <robinmacisland@gmail.com>
2023-02-09 17:10:46 +0300

    this file is not still working

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -1,22 +1,22 @@
-
-var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// var map = L.map('map').setView([51.505, -0.09], 13);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+// , {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
 
 
 // fetching the incidents
 const ab = () => {
-    const incidents = fetch(`http://localhost:8080/incidents/listincidents`);
+    const incidents = fetch(`http://localhost:8080/incidents/listincidents
+`);
     incidents.then(data => 
             data.json().then(incidents => {
                 incidents.forEach(element => {
-                    const {cause, startDate, site} = element;
-                    const longitude = element.site;
-                    console.log(`${cause} - ${startDate}`)
+                    const {cause, startDate} = element;
+                    console.log(`${cause} - ${startDate} - ${element.site.longitude}`)
                     
-                    var marker = L.marker([51.5, -0.09]).addTo(map);
+                    // var marker = L.marker([51.5, -0.09]).addTo(map);
 
 
                 });
