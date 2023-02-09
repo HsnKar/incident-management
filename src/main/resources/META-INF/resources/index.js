436c6ca7aaae710d7c13fcad0788f353ca5a0fa9
HsnKar <robinmacisland@gmail.com>
2023-02-09 10:05:52 +0300

    Added this file for the map

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -1,17 +1,27 @@
-// Initialize and add the map
-function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
-    });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap;
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// fetching the incidents
+const ab = () => {
+    const incidents = fetch(`http://localhost:8080/incidents/listincidents`);
+    incidents.then(data => 
+            data.json().then(incidents => {
+                incidents.forEach(element => {
+                    const {cause, startDate, site} = element;
+                    const longitude = element.site;
+                    console.log(`${cause} - ${startDate}`)
+                    
+                    var marker = L.marker([51.5, -0.09]).addTo(map);
+
+
+                });
+            })
+        ).catch(error => console.log('There was an error with the fetch operation'));
+};
+
+ab();
