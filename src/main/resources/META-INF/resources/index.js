70d4c138d09d6e7a5cf577d10fd912c47ba6072c
HsnKar <robinmacisland@gmail.com>
2023-02-10 16:57:20 +0300

    It's finally working. I'm happy :)

--- src/main/resources/META-INF/resources/index.js
+++ src/main/resources/META-INF/resources/index.js
@@ -1,9 +1,11 @@
-// var map = L.map('map').setView([51.505, -0.09], 13);
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-// , {
-//     maxZoom: 19,
-//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-// }).addTo(map);
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+, {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
 
 
 // fetching the incidents
@@ -14,9 +16,12 @@ const ab = () => {
             data.json().then(incidents => {
                 incidents.forEach(element => {
                     const {cause, startDate} = element;
-                    console.log(`${cause} - ${startDate} - ${element.site.longitude}`)
+                    var lat = element.site.latitude;
+                    var lgt = element.site.longitude;
+                    console.log(`${cause} - ${startDate} - ${element.site.longitude} - ${element.site.latitude}`)
                     
                     // var marker = L.marker([51.5, -0.09]).addTo(map);
+                    var marker = L.marker([lat, lgt]).addTo(map);
 
 
                 });
@@ -25,3 +30,7 @@ const ab = () => {
 };
 
 ab();
+
+var satellite = L.tileLayer(mapboxUrl, {id: 'MapID', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
+
+layerControl.addBaseLayer(satellite, "Satellite");
