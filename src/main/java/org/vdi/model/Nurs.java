7b2029887665d9a624f9c45cbf236a97df7d5752
HsnKar <robinmacisland@gmail.com>
2023-02-28 16:26:26 +0300

    This is the master class for the General NURs

---
+++ src/main/java/org/vdi/model/Nurs.java
@@ -0,0 +1,45 @@
+package org.vdi.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "nurs")
+public class Nurs extends PanacheEntityBase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    @Column(name = "name")
+    public String name;
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "nur")
+    public List<Nur> nur;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Nur> getNur() {
+        return nur;
+    }
+
+    public void setNur(List<Nur> nur) {
+        this.nur = nur;
+    }
+}
