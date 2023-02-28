6e7479e77b4ed85e1ebc09fa069c3bd479b4c11e
HsnKar <robinmacisland@gmail.com>
2023-02-28 16:27:20 +0300

    This class is made to handle all the reporting stuff

---
+++ src/main/java/org/vdi/controller/Report.java
@@ -0,0 +1,31 @@
+package org.vdi.controller;
+
+import com.oracle.svm.core.annotate.Inject;
+import io.quarkus.qute.Location;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import org.vdi.model.Service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/reporting")
+public class Report {
+//    @Inject
+//    Template getreporting;
+//    @Inject
+//    Template accueilService;
+
+    @Location("getreporting.html")
+    Template getreporting;
+
+    @GET
+    public TemplateInstance getServiceAccueil() {
+        org.vdi.model.Service service = new org.vdi.model.Service();
+        List<Service> services = new ArrayList<>();
+        return getreporting.data("services", services);
+    }
+
+}
